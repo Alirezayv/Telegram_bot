228f6a9e59560d14be1a205aefe681d313ebd4a5
root <root@vps.server.local>
2022-11-26 14:52:59 +0000

    second commit

--- main.py
+++ main.py
@@ -20,15 +20,18 @@ my_bot = Bot(token ='5030895573:AAH63KvuN-Td40DCcVjGhLP9sZqs97V19yM')
 textChannel_ID = "-1001168876759"
 
 register = """
-برای ثبت نام، نام و نام خانوادگی خود را وارد کنید.
+برای ادامه ثبت روی ارسال شماره کلیک کنید.
 """
 
-sendingPhone = "ارسال شماره تلفن"
-
-registerd = """
-شما عضو شدید
+send_num = """
+ارسال شماره
 """
 
+mexc_url ='https://www.mexc.com/landings/englishirannewuser12?handleDefaultLocale=keep&inviteCode=mexc-1Wb9n'
+
+mexc_register = 'برای ادامه ثبت نام میتوانید از طریق لینک زیر و یا کد mexc-1Wb9n  در صرافی مکسی ثبت نام کنید و سپس روی انجام شد کلیک کنید.'
+mexc_reg = 'ثبت نام در مکسی'
+done_reg = 'انجام شد'
 user_data =  {
 
         "user_name" : "",
@@ -36,21 +39,24 @@ user_data =  {
     }
 def start(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
-    context.bot.sendMessage(chat_id, "What is your name?")
+    buttons = [
+        [KeyboardButton(send_num, request_contact= True)]
+    ]
+    context.bot.sendMessage(chat_id=chat_id, text=register, reply_markup = ReplyKeyboardMarkup(buttons, resize_keyboard= True, one_time_keyboard=True))
 
-def get_phone(update: Update, context: CallbackContext):
+def mexc(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
-    user_data["user_name"] = update.message.text
-    context.bot.sendMessage(chat_id, user_data['user_name'])
-
+    user_data["user_phone"] = update.message.contact.phone_number
+    print(user_data['user_phone'])
+    register_but = [
+            [InlineKeyboardButton(text=mexc_reg, url=mexc_url)],
+            [InlineKeyboardButton(text=done_reg, url=mexc_url)]
+    ]
+    context.bot.sendMessage(chat_id=chat_id, text=mexc_register, reply_markup = InlineKeyboardMarkup(register_but))
 
 start_handler = CommandHandler('start', start) 
-
-txt_handler = MessageHandler(Filters.chat_type.private, get_phone)
-
-
+mexc_handler = MessageHandler (Filters.contact,callback=mexc, pass_user_data=True)
+dispatcher.add_handler(mexc_handler)
 dispatcher.add_handler(start_handler)
-dispatcher.add_handler(txt_handler)
-
 updater.start_polling()
 updater.idle()
